4db518872bc796c28e0a53917dcc577d098d79a7
nicolasbojacaro-enfinal_metodos-a3htoexy <nicolasbojacaro-enfinal_metodos-a3htoexy@local>
2018-05-25 00:04:28 +0000

    primera version

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,9 +2,14 @@
 # Complete el siguiente codigo para recorrer la lista `x` e imprima
 # los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
 import numpy as np
-
 x = np.int_(np.random.random(100)*1000)
-print(x)
+for i in range(len(x)):
+    if (x[i]<=800):
+        if(x[i]%2!=0):
+            print(x[i])
+    else:
+        break
+        
 
 
 
