33c592f258589833e8eeab55099b4956dbb06f87
nicolasbojacaro-enfinal_metodos-a3htoexy <nicolasbojacaro-enfinal_metodos-a3htoexy@local>
2018-05-25 00:43:39 +0000

    segunda version

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,6 +4,23 @@
 # Calcule la covarianza entre `u` y `v` e imprima su valor.
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+def cov(u, v):
+    pu = np.mean(u)
+    pv = np.mean(v)
+    puntos = len(u)
+    varuv = np.sum((u-pu)*(v-pv))/puntos
+    return varuv
+plt.plot(t, v, label = "serie de v")
+plt.plot(t, u, label = 'serie de u')
+plt.legend(loc = 0)
+plt.savefig('serie.png')
+print (cov(u,v))
+    
+    
