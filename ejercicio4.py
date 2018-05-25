d5eb150e9e711a2223f805a3e088ac4a812ca793
nicolasbojacaro-enfinal_metodos-a3htoexy <nicolasbojacaro-enfinal_metodos-a3htoexy@local>
2018-05-25 00:56:52 +0000

    cuarta version

--- ejercicio4.py
+++ ejercicio4.py
@@ -7,7 +7,9 @@
 # la transformada inversa para graficar la nueva senal (figura 'filtro.png')
 
 import numpy as np
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 n = 512 # number of point in the whole interval
 f = 200.0 #  frequency in Hz
@@ -16,3 +18,6 @@ t = np.linspace( 0, (n-1)*dt, n)
 y = np.sin(2 * np.pi * f * t) + np.cos(2 * np.pi * f * t * t)
 noise = 1.4*(np.random.rand(n)+0.7)
 y  =  y + noise
+plt.plot(y, t)
+plt.savefig('senal.png')
+
--- ejercicio5.py
+++ ejercicio5.py
@@ -7,9 +7,12 @@
 # condiciones iniciales t=0, x=0.0, y=0.0, z=0.0, hasta t=5.0.
 # Prepare dos graficas con la solucion: de x vs y (xy.png), x vs. z (xz.png) 
 
-
-
-
-
-
-
+sigma = 10
+beta = 2.67
+rho = 28.0
+dx/dt = sigma*(y-x)
+dy/dt = rho * x - y - x * z
+dz/dt = -beta * z + x * y
+x0=0
+y0=0
+t0=0
